d79e1a5ef3c0cab1cbc552a2d129b9cb8dae1a46
anjali <anjalipaswan@innotechteam.in>
2023-01-13 12:38:03 +0530

    completed mapping

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Counter from "./Counter";
+// import Home from "./Home";
+// import AddBtn from "./AddBtn";
 
 export default function App() {
   return (
     <div>
       <Counter />
+      {/* <Home></Home> */}
+      {/* <AddBtn></AddBtn> */}
     </div>
   );
 }
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,21 +1,59 @@
 import { useState } from "react";
 import "./counter.css";
-function Counter() {
-  const [counters, setCounters] = useState("");
 
-  const addCounter = () => {
-    setCounters([...counters]);
-  };
+function Counter() {
+  const [counters, setCounters] = useState([{ id: 1, value: 0 }]);
+
+  function create() {
+    setCounters([...counters, { id: counters.length + 1, value: 0 }]);
+  }
+
+  function increase(id) {
+    setCounters(
+      counters.map((counter) => {
+        if (counter.id === id) {
+          return { id, value: counter.value + 1 };
+        }
+        return counter;
+      })
+    );
+  }
+
+  function reset(id) {
+    setCounters(
+      counters.map((counter) => {
+        if (counter.id === id) {
+          return { id, value: 0 };
+        }
+        return counter;
+      })
+    );
+  }
 
   return (
-    <div>
-      <div className="counter-header">
-        <button className="counter-button" onClick={addCounter}>
+    <div className="counter">
+      <div className="counter-add">
+        <button type="button" className="add-button" onClick={create}>
           Add Counter
         </button>
-        <button className="start-stop-button"></button>
-
-        <button className="pluse-button">0</button>
+      </div>
+      <div>
+        {counters.map((counter) => (
+          <div className="counter-container" key={counter.id}>
+            <div className="counter-output">{counter.value}</div>
+            <div className="btn__container">
+              <button
+                className="control__btn"
+                onClick={() => increase(counter.id)}
+              >
+                Start Counter
+              </button>
+              <button className="reset" onClick={() => reset(counter.id)}>
+                Reset
+              </button>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
