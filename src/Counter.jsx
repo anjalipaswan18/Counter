ecb71756aa80a083fcc6e3040e091beee1f9975f
anjali <anjalipaswan@innotechteam.in>
2023-01-16 10:58:30 +0530

    Completed add-counter

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,48 +1,36 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./counter.css";
 function Counter() {
   const [total, setTotal] = useState(0);
   const [counters, setCounters] = useState([]);
   function create() {
-    setCounters([
-      ...counters,
-      { id: counters.length + 1, count: 0, started: false },
-    ]);
-  }
-  let intervalId;
-  const StartStop = (id) => {
-    const NewCounter = counters.map((counter) => {
-      if (counter.id === id) {
-        counter.started = !counter.started;
-        if (counter.started) {
-          intervalId = setInterval(() => {
-            Increase(counter.id);
-          }, 1000);
-        } else {
-          clearInterval(intervalId);
-        }
-      }
-      return counter;
-    });
-    setCounters(NewCounter);
-  };
-
-  useEffect(() => {
-    return () => {
-      clearInterval(intervalId);
+    var newCounter = {
+      id: counters.length + 1,
+      count: 0,
+      started: false,
+      intervalId: "",
     };
-  }, [intervalId]);
-
-  const Increase = (id) => {
-    const NewCounter = counters.map((counter) => {
-      if (counter.id === id) {
-        counter.count++;
+    setCounters([...counters, newCounter]);
+    console.log(newCounter);
+  }
+  const StartStop = (id, intervalId) => {
+    let singleItem = counters.filter((item) => {
+      if (item.id === id) {
+        return item;
       }
-      return counter;
     });
-    setCounters(NewCounter);
-    setTotal(NewCounter.reduce((total, counter) => total + counter.count, 0));
-    console.log(NewCounter);
+    if (singleItem[0].started === false) {
+      intervalId = setInterval(() => {
+        singleItem[0].count++;
+        singleItem[0].started = true;
+        singleItem[0].intervalId = intervalId;
+        setTotal(counters.reduce((total, counter) => total + counter.count, 0));
+      }, 1000);
+    } else if (singleItem[0].started === true) {
+      clearInterval(intervalId);
+      singleItem[0].started = false;
+      singleItem[0].intervalId = null;
+    }
   };
   return (
     <div className="counter">
@@ -50,28 +38,39 @@ function Counter() {
         <button type="button" className="add-button" onClick={create}>
           Add Counter
         </button>
+        <div className="total-reset-container">
+          <div className="total-container">{total}</div>
+        </div>
       </div>
-      <div>
-        {counters.map((counter) => (
-          <div className="counter-container" key={counter.id}>
-            <div className="counter-output">{counter.count}</div>
+      <div className="counter-main">
+        {counters.map((item) => (
+          <div key={item.id} className="counter-container">
+            <div className="counter-header">Counter {item.id}</div>
+            <div className="counter-output">{item.count}</div>
             <div className="btn__container">
-              <div className="counter" key={counter.id}>
-                <div className="counter-header">
-                  Counter {counter.id}:
-                  <button
-                    className="start-stop-button"
-                    onClick={() => StartStop(counter.id)}
-                  >
-                    {counter.started ? "Stop" : "Start"}
-                  </button>
-                </div>
-              </div>
+              {!item.started ? (
+                <span
+                  onClick={() =>
+                    StartStop(item.id, item.intervalId, item.started)
+                  }
+                  className="start-button"
+                >
+                  Start
+                </span>
+              ) : (
+                <span
+                  onClick={() =>
+                    StartStop(item.id, item.intervalId, item.started)
+                  }
+                  className="stop-button"
+                >
+                  Stop
+                </span>
+              )}
             </div>
           </div>
         ))}
       </div>
-      <div className="total-container">Total: {total}</div>
     </div>
   );
 }
